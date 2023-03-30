d259d4d4c3de420022b15c8717e1cac36da8d0eb
Ryan McPeck <mcoso.dev@gmail.com>
2023-03-30 13:04:26 -0400

    feat: update deploy script

--- deploy/00_main.ts
+++ deploy/00_main.ts
@@ -5,8 +5,33 @@ export default async function deploy(hardhat: HardhatRuntimeEnvironment) {
 
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
+
   await deploy("ERC20Manager", {
     contract: "ERC20Manager",
+    from: deployer,
+    args: [],
+    skipIfAlreadyDeployed: false,
+    log: true,
+  });
+
+  await deploy("ERC20FromAllowanceEnforcer", {
+    contract: "ERC20FromAllowanceEnforcer",
+    from: deployer,
+    args: [],
+    skipIfAlreadyDeployed: false,
+    log: true,
+  });
+
+  await deploy("TimestampBeforeEnforcer", {
+    contract: "TimestampBeforeEnforcer",
+    from: deployer,
+    args: [],
+    skipIfAlreadyDeployed: false,
+    log: true,
+  });
+
+  await deploy("TimestampAfterEnforcer", {
+    contract: "TimestampAfterEnforcer",
     from: deployer,
     args: [],
     skipIfAlreadyDeployed: false,
